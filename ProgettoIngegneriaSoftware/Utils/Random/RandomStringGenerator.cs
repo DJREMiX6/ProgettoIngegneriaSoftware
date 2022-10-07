0bfb4473a24d9f43669787e8c881459ec96d55c4
DJ BUGI <francescogirellilime@gmail.com>
2022-10-07 18:20:31 +0200

    Created a Random class that generates random strings

---
+++ ProgettoIngegneriaSoftware/Utils/Random/RandomStringGenerator.cs
@@ -0,0 +1,34 @@
+﻿namespace ProgettoIngegneriaSoftware.Utils.Random
+{
+    public class RandomStringGenerator
+    {
+
+        #region STATIC SINGLETON
+
+        private static RandomStringGenerator _instance;
+        public static RandomStringGenerator Shared => _instance ??= new RandomStringGenerator();
+
+        #endregion STATIC SINGLETON
+
+        #region PRIVATE PROPS
+
+        private System.Random random => System.Random.Shared;
+
+        #endregion PRIVATE PROPS
+
+        #region PUBLIC METHODS
+
+        public string RandomString(int length)
+        {
+            var str = string.Empty;
+            for(int i = 0; i < length; i++)
+            {
+                str += Convert.ToChar(random.Next(0, 26));
+            }
+            return str;
+        }
+
+        #endregion PUBLIC PROPS
+
+    }
+}
