638ed54d6b776110a423c4e90335f566a18c1a3d
DJ BUGI <francescogirellilime@gmail.com>
2022-10-11 14:22:28 +0200

    Added a string extension method for the comparison

--- ProgettoIngegneriaSoftware/Utils/Extensions/BaseTypeExtensions.cs
+++ ProgettoIngegneriaSoftware/Utils/Extensions/BaseTypeExtensions.cs
@@ -2,6 +2,13 @@
 {
     public static class BaseTypeExtensions
     {
+
+        /// <summary>
+        /// Does a comparison byte per byte of the two arrays
+        /// </summary>
+        /// <param name="byteArray1"></param>
+        /// <param name="byteArray2"></param>
+        /// <returns><c>true</c> if every byte of <c>byteArray1</c> is equal and in the same position inside of <c>byteArray2</c></returns>
         public static bool EqualsByByte(this byte[] byteArray1, byte[] byteArray2)
         {
             if (byteArray1.Length != byteArray2.Length)
@@ -17,5 +24,28 @@
             }
             return true;
         }
+
+        /// <summary>
+        /// Checks if a <seealso cref="string"/> contains at least one <seealso cref="char"/> from a <seealso cref="char"/> array
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="chars"></param>
+        /// <returns><c>true</c> if the <seealso cref="string"/> contains at least on of the <seealso cref="char"/> from the <seealso cref="char"/> array. 
+        /// Otherwise it returns <c>false</c></returns>
+        public static bool Contains(this string str, char[] chars)
+        {
+            foreach(char stringChar in str)
+            {
+                foreach(char c in chars)
+                {
+                    if(stringChar.Equals(c))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
     }
 }
