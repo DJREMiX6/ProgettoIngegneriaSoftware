61c504a5c307af2d4d0f807ac6df88f6f3bb5200
Francesco Girelli <francescogirellilime@gmail.com>
2022-10-12 17:34:55 +0200

    InternalServerError Response Data and ObjectResult added to the project

---
+++ ProgettoIngegneriaSoftware/Models/HttpResponseObjects/InternalServerErrorObjectResult.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Net;
+
+namespace ProgettoIngegneriaSoftware.Models.HttpResponseObjects
+{
+    public class InternalServerErrorObjectResult : ObjectResult
+    {
+        public InternalServerErrorObjectResult([ActionResultObjectValue] object? value) : base(value)
+        {
+            base.StatusCode = (int)HttpStatusCode.InternalServerError;
+        }
+
+        public InternalServerErrorObjectResult([ActionResultObjectValue] ModelStateDictionary modelState) : base(modelState)
+        {
+            base.StatusCode = (int)HttpStatusCode.InternalServerError;
+        }
+    }
+}
