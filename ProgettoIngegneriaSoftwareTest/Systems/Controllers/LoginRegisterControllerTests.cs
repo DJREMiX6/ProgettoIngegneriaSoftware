76478a377248547d0f4f0a1dc9e35d2ec93d1d76
DJ BUGI <francescogirellilime@gmail.com>
2022-11-02 18:24:23 +0100

    Added Test project with reference to ProgettoIngegneriaSoftware project

---
+++ ProgettoIngegneriaSoftwareTest/Systems/Controllers/LoginRegisterControllerTests.cs
@@ -0,0 +1,15 @@
+using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProgettoIngegneriaSoftware.Controllers;
+using ProgettoIngegneriaSoftware.Models.ControllersModels;
+using ProgettoIngegneriaSoftware.Models.DB_Models.Autentication;
+using ProgettoIngegneriaSoftware.Models.DB_Models.Autentication.Records;
+
+namespace ProgettoIngegneriaSoftware.Test.Systems.Controllers
+{
+    public class LoginRegisterControllerTests
+    {
+
+    }
+}
