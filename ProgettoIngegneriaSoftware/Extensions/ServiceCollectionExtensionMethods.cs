855a3287bf4918beecbc4efa2c8340461f4f658e
DJ BUGI <francescogirellilime@gmail.com>
2022-11-02 18:45:20 +0100

    Extracted ServiceCollection methods to group them in ServiceCollectionExtensionMethods

--- ProgettoIngegneriaSoftware/Extensions/ServiceCollectionExtensionMethods.cs
+++ ProgettoIngegneriaSoftware/Extensions/ServiceCollectionExtensionMethods.cs
@@ -1,4 +1,6 @@
-﻿using ProgettoIngegneriaSoftware.Models.ControllersModels;
+﻿using Microsoft.EntityFrameworkCore;
+using ProgettoIngegneriaSoftware.Models;
+using ProgettoIngegneriaSoftware.Models.ControllersModels;
 using ProgettoIngegneriaSoftware.Security;
 using ProgettoIngegneriaSoftware.Security.Argon2;
 using ProgettoIngegneriaSoftware.Services;
@@ -7,6 +9,20 @@ namespace ProgettoIngegneriaSoftware.Extensions
 {
     public static class ServiceCollectionExtensionMethods
     {
+
+        public static IServiceCollection AddDatabaseContext(this IServiceCollection serviceCollection)
+        {
+
+#if DEBUG
+            serviceCollection.AddDbContext<AutenticationDbContext>(options => options.UseInMemoryDatabase("Test_ProgettoIngegneria_AutenticationDb"));
+            serviceCollection.AddDatabaseDeveloperPageExceptionFilter();
+#else
+            serviceCollection.AddDbContext<AutenticationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AutenticationDefaultConnection")));
+#endif
+
+            return serviceCollection;
+        }
+
         public static IServiceCollection AddServices(this IServiceCollection serviceCollection)
         {
             serviceCollection.AddTransient<IUserModelService, UserModelService>();
--- ProgettoIngegneriaSoftware/Program.cs
+++ ProgettoIngegneriaSoftware/Program.cs
@@ -1,11 +1,8 @@
 using Serilog;
 using Serilog.Events;
 using Microsoft.EntityFrameworkCore;
+using ProgettoIngegneriaSoftware.Extensions;
 using ProgettoIngegneriaSoftware.Models;
-using ProgettoIngegneriaSoftware.Security;
-using ProgettoIngegneriaSoftware.Security.Argon2;
-using ProgettoIngegneriaSoftware.Models.Tokenization;
-using ProgettoIngegneriaSoftware.Models.ControllersModels;
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
@@ -19,13 +16,10 @@ builder.Configuration.AddJsonFile("appsettings.json");
 builder.Configuration.AddJsonFile("appsettings.development.json");
 builder.Logging.ClearProviders();
 builder.Logging.AddSerilog();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDefaultConnection")));
-builder.Services.AddDbContext<AutenticationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AutenticationDefaultConnection")));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddTransient<ITokenGenerator, TokenGenerator>();
-builder.Services.AddTransient<IPasswordHasher, Argon2PasswordHasher>();
-builder.Services.AddTransient<IPasswordHasherOptions, Argon2PasswordHasherOptions>();
-builder.Services.AddTransient<ILoginRegisterModel, LoginRegisterModel>();
+builder.Services.AddDatabaseContext();
+builder.Services.AddServices();
+builder.Services.AddPasswordHasher();
+builder.Services.AddModels();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();   // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
