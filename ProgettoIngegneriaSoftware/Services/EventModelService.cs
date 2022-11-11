bcda112e7ecfb0f0a2a66fbe742d024c3b70a760
DJ BUGI <francescogirellilime@gmail.com>
2022-11-11 17:56:26 +0100

    Created Event Service and its abstraction

---
+++ ProgettoIngegneriaSoftware/Services/EventModelService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgettoIngegneriaSoftware.Models;
+using ProgettoIngegneriaSoftware.Models.DB_Models.Application;
+using ProgettoIngegneriaSoftware.Models.DB_Models.Application.Abstraction;
+
+namespace ProgettoIngegneriaSoftware.Services
+{
+    public class EventModelService : IEventModelService
+    {
+
+        #region PRIVATE READONLY DI FIELDS
+
+        private readonly ILogger<EventModelService> _logger;
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        #endregion PRIVATE READONLY DI FIELDS
+
+        #region CTORS
+
+        public EventModelService(ILogger<EventModelService> logger, ApplicationDbContext applicationDbContext)
+        {
+            _logger = logger;
+            _applicationDbContext = applicationDbContext;
+        }
+
+        #endregion CTORS
+
+        #region CREATE
+
+        public async Task<IReadableEventModel?> CreateAsync(IEditableEventModel editableEventModel, Guid adminId)
+        {
+            var eventToCreate = new EventModel(editableEventModel);
+
+            await _applicationDbContext.Events.AddAsync(eventToCreate);
+            await _applicationDbContext.SaveChangesAsync();
+
+            return eventToCreate;
+        }
+
+        #endregion CREATE
+
+        #region READ
+
+        public async Task<ICollection<IReadableEventModel>> GetAsync()
+        {
+            var eventsList = await _applicationDbContext.Events.ToListAsync();
+            return eventsList.ToArray();
+        }
+
+        public async Task<IReadableEventModel?> GetAsync(Guid id)
+        {
+            return await _applicationDbContext.Events.FindAsync(id);
+        }
+
+        #endregion READ
+
+        #region UPDATE
+
+
+
+        #endregion UPDATE
+
+        #region DELETE
+
+
+
+        #endregion DELETE
+
+    }
+}
