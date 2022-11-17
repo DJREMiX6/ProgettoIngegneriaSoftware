ac5db5a334b76a59d93cefb6e0ab7f14f5005753
alextilea98 <alexandrumihai.tilea@studenti.unipg.it>
2022-11-17 03:27:47 +0100

    Created a utility HttpRequest extension method to get the base url of the application

---
+++ ProgettoIngegneriaSoftware/Extensions/HttpRequestExtensionMethods.cs
@@ -0,0 +1,8 @@
+﻿namespace ProgettoIngegneriaSoftware.Extensions
+{
+    public static class HttpRequestExtensionMethods
+    {
+        public static string GetBaseUrl(this HttpRequest request) => string.Format("{0}://{1}{2}/", request.Scheme,
+            request.Host.Host, request.Host.Port is null ? string.Empty : ":" + request.Host.Port);
+    }
+}
