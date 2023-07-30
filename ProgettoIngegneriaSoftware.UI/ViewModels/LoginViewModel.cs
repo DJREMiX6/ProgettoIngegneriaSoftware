899f15269b9ae67909e8be7fb486df1ef451718a
Francesco Girelli <francesco.girelli@protonmail.com>
2023-07-30 14:48:46 +0200

    Created a login page

--- ProgettoIngegneriaSoftware.UI/ViewModels/LoginViewModel.cs
+++ ProgettoIngegneriaSoftware.UI/ViewModels/LoginViewModel.cs
@@ -2,7 +2,10 @@
 using CommunityToolkit.Maui.Core;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using ProgettoIngegneriaSoftware.UI.Services.AuthenticationService;
+using ProgettoIngegneriaSoftware.UI.Exceptions.API;
+using ProgettoIngegneriaSoftware.UI.Models;
+using ProgettoIngegneriaSoftware.UI.Services.ApiHttpClient;
+using ProgettoIngegneriaSoftware.UI.Views;
 
 namespace ProgettoIngegneriaSoftware.UI.ViewModels;
 
@@ -10,22 +13,22 @@ public partial class LoginViewModel : BaseViewModel
 {
 
     #region FIELDS
-
-    private readonly IAuthenticationService _authenticationService;
-
-    [ObservableProperty]
-    private string _email;
+    
+    private readonly ApiHttpClient _httpClient;
 
     [ObservableProperty]
-    private string _password;
+    private string _email = "pisello@palle.com"; //TODO REMOVE
+
+    [ObservableProperty]
+    private string _password = "PiselloPalle22!"; //TODO REMOVE
 
     #endregion FIELDS
 
     #region CTORS
 
-    public LoginViewModel(IAuthenticationService authenticationService)
+    public LoginViewModel(ApiHttpClient httpClient)
     {
-        _authenticationService = authenticationService;
+        _httpClient = httpClient;
         Title = "Sign In";
     }
 
@@ -36,8 +39,44 @@ public partial class LoginViewModel : BaseViewModel
     [RelayCommand]
     private async Task Login()
     {
-        await Toast.Make("TestMessage", ToastDuration.Long, textSize: 24d).Show();
+        if (IsBusy) return;
 
+        IsBusy = true;
+
+        IToast? toast;
+        try
+        {
+            var loginUserInfo = new LoginUserInfo()
+            {
+                Email = Email,
+                Password = Password
+            };
+
+            await _httpClient.SignInAsync(loginUserInfo);
+            toast = Toast.Make("Successfully signed in.", ToastDuration.Long, textSize: 18d);
+        }
+        catch (UnauthorizedApiException unauthorizedApiException)
+        {
+            toast = Toast.Make(unauthorizedApiException.Message, ToastDuration.Long, textSize: 18d);
+        }
+        catch (BadRequestApiException badRequestApiException)
+        {
+            toast = Toast.Make(badRequestApiException.Message, ToastDuration.Long, textSize: 18d);
+        }
+        /*catch (Exception exception)
+        {
+            toast = Toast.Make(exception.Message, ToastDuration.Long, textSize: 18d);
+        }*/
+
+        await toast.Show();
+
+        IsBusy = false;
+    }
+
+    [RelayCommand] 
+    private async Task NavigateToRegisterPage()
+    {
+        await Shell.Current.GoToAsync(nameof(RegisterPage), animate: true);
     }
 
     #endregion COMMANDS
