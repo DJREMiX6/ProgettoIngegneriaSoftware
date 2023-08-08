8d5f74710132e755967b03939d6c98b91609741c
Francesco Girelli <francesco.girelli@protonmail.com>
2023-08-08 13:06:56 +0200

    Added SignUp and SignOut methods to the ApiHttpClient

--- ProgettoIngegneriaSoftware.UI/Services/ApiHttpClient/ApiHttpClient.cs
+++ ProgettoIngegneriaSoftware.UI/Services/ApiHttpClient/ApiHttpClient.cs
@@ -24,37 +24,85 @@ public class ApiHttpClient : HttpClient
     }
 
     #endregion CTORS
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="loginUserInfo"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
-    /// <exception cref="UnauthorizedApiException"></exception>
-    /// <exception cref="BadRequestApiException"></exception>
-    /// <exception cref="ArgumentException"></exception>
+    
     public async Task SignInAsync(ILoginUserInfo loginUserInfo, CancellationToken? cancellationToken = null)
     {
         var uri = _uriService.AuthenticationSignInPath();
         var httpResponseMessage = await this.PostAsJsonAsync(uri, loginUserInfo,
             cancellationToken ?? new CancellationToken(false));
 
-        if (httpResponseMessage.StatusCode == HttpStatusCode.OK)
+        if (httpResponseMessage.IsSuccessStatusCode)
         {
             return;
         }
-        if(httpResponseMessage.StatusCode == HttpStatusCode.Unauthorized)
+        switch (httpResponseMessage.StatusCode)
         {
-            var problemDetails = await httpResponseMessage.Content.ReadFromJsonAsync<ProblemDetails>();
-            throw new UnauthorizedApiException(problemDetails?.Detail);
+            case HttpStatusCode.Unauthorized:
+            {
+                var problemDetails = await httpResponseMessage.Content.ReadFromJsonAsync<ProblemDetails>();
+                throw new UnauthorizedApiException(problemDetails?.Detail);
+            }
+            case HttpStatusCode.BadRequest:
+            {
+                var problemDetails = await httpResponseMessage.Content.ReadFromJsonAsync<ProblemDetails>();
+                throw new BadRequestApiException(problemDetails?.Detail);
+            }
+            default:
+                throw new ArgumentException($"Unexpected error! Status code: {httpResponseMessage.StatusCode}.", nameof(httpResponseMessage.StatusCode));
         }
-        if (httpResponseMessage.StatusCode == HttpStatusCode.BadRequest)
-        {
-            var problemDetails = await httpResponseMessage.Content.ReadFromJsonAsync<ProblemDetails>();
-            throw new BadRequestApiException(problemDetails?.Detail);
-        }
-
-        throw new ArgumentException($"Unexpected error! Status code: {httpResponseMessage.StatusCode}.", nameof(httpResponseMessage.StatusCode));
     }
+
+    public async Task SignUpAsync(IRegisterUserInfo registerUserInfo, CancellationToken? cancellationToken = null)
+    {
+        var uri = _uriService.AuthenticationSignUpPath();
+        var httpResponseMessage = await this.PostAsJsonAsync(uri, registerUserInfo, 
+            cancellationToken ?? new CancellationToken(false));
+
+        if (httpResponseMessage.IsSuccessStatusCode)
+        {
+            return;
+        }
+        switch (httpResponseMessage.StatusCode)
+        {
+            case HttpStatusCode.Unauthorized:
+            {
+                var problemDetails = await httpResponseMessage.Content.ReadFromJsonAsync<ProblemDetails>();
+                throw new UnauthorizedApiException(problemDetails?.Detail);
+            }
+            case HttpStatusCode.BadRequest:
+            {
+                var problemDetails = await httpResponseMessage.Content.ReadFromJsonAsync<ProblemDetails>();
+                throw new BadRequestApiException(problemDetails?.Detail);
+            }
+            default:
+                throw new ArgumentException($"Unexpected error! Status code: {httpResponseMessage.StatusCode}.", nameof(httpResponseMessage.StatusCode));
+        }
+    }
+
+    public async Task SignOut(CancellationToken? cancellationToken = null)
+    {
+        var uri = _uriService.AuthenticationSignOutPath();
+        var httpResponseMessage = await this.PostAsync(uri, null, cancellationToken ?? new CancellationToken(false));
+
+        if (httpResponseMessage.IsSuccessStatusCode)
+        {
+            return;
+        }
+        switch (httpResponseMessage.StatusCode)
+        {
+            case HttpStatusCode.Unauthorized:
+            {
+                var problemDetails = await httpResponseMessage.Content.ReadFromJsonAsync<ProblemDetails>();
+                throw new UnauthorizedApiException(problemDetails?.Detail);
+            }
+            case HttpStatusCode.BadRequest:
+            {
+                var problemDetails = await httpResponseMessage.Content.ReadFromJsonAsync<ProblemDetails>();
+                throw new BadRequestApiException(problemDetails?.Detail);
+            }
+            default:
+                throw new ArgumentException($"Unexpected error! Status code: {httpResponseMessage.StatusCode}.", nameof(httpResponseMessage.StatusCode));
+        }
+    }
+
 }
