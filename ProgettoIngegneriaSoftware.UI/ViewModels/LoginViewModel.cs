cdda3c225dd8137f81d98849e331de8da56efa7e
Francesco Girelli <francesco.girelli@protonmail.com>
2023-08-08 13:07:50 +0200

    Added login auto redirection if already logged in or if logged in successfully

--- ProgettoIngegneriaSoftware.UI/ViewModels/LoginViewModel.cs
+++ ProgettoIngegneriaSoftware.UI/ViewModels/LoginViewModel.cs
@@ -44,6 +44,7 @@ public partial class LoginViewModel : BaseViewModel
         IsBusy = true;
 
         IToast? toast;
+        var redirect = false;
         try
         {
             var loginUserInfo = new LoginUserInfo()
@@ -53,22 +54,31 @@ public partial class LoginViewModel : BaseViewModel
             };
 
             await _httpClient.SignInAsync(loginUserInfo);
+            redirect = true;
             toast = Toast.Make("Successfully signed in.", ToastDuration.Long, textSize: 18d);
         }
         catch (UnauthorizedApiException unauthorizedApiException)
         {
+            redirect = true;
             toast = Toast.Make(unauthorizedApiException.Message, ToastDuration.Long, textSize: 18d);
         }
         catch (BadRequestApiException badRequestApiException)
         {
             toast = Toast.Make(badRequestApiException.Message, ToastDuration.Long, textSize: 18d);
         }
-        /*catch (Exception exception)
+        catch (Exception exception)
         {
+#if DEBUG
             toast = Toast.Make(exception.Message, ToastDuration.Long, textSize: 18d);
-        }*/
+#else
+            toast = Toast.Make("Unknown error.", ToastDuration.Long, textSize: 18d);
+#endif
+        }
 
         await toast.Show();
+
+        if (redirect)
+            await NavigateToMainPage();
 
         IsBusy = false;
     }
@@ -76,9 +86,20 @@ public partial class LoginViewModel : BaseViewModel
     [RelayCommand] 
     private async Task NavigateToRegisterPage()
     {
+        IsBusy = true;
         await Shell.Current.GoToAsync(nameof(RegisterPage), animate: true);
+        IsBusy = false;
     }
 
     #endregion COMMANDS
+
+    #region METHODS
+
+    private async Task NavigateToMainPage()
+    {
+        await Shell.Current.GoToAsync(nameof(EventsPage), animate: true);
+    }
+
+    #endregion METHODS
 
 }
